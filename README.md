79fd55c70e2d435aaadf4fb25e01bc179f2fb6f6
AnupVeerapur <anupsveerapur@gmail.com>
2020-04-23 14:47:35 +0530

    Loader

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # tila
 
 # how to run the project
-. Step 1 - npm install
-. step 2 - npm run dev
-. Open : http://localhost:3000/
+* Step 1 - npm install
+* step 2 - npm run dev
+* Open : http://localhost:3000/
--- containers/compareList.js
+++ containers/compareList.js
@@ -3,7 +3,7 @@ import { Checkbox, Row, Col, Select } from 'antd';
 import actions from "../redux/action";
 import { connect } from "react-redux";
 import CompareOf from "../components/compareOf";
-import { CloseCircleOutlined } from '@ant-design/icons';
+import { CloseCircleOutlined, LoadingOutlined } from '@ant-design/icons';
 
 const { onFetchItem } = actions;
 
@@ -70,102 +70,113 @@ class CompareList extends Component {
         let f1 = this.props.featureListData;
         let c1 = this.props.compareListData;
         let { rightItem, rightDropdown, leftDropdown, leftItem, countSel, showOnlyDiff } = this.state;
+        console.log("f111111111111111", f1)
         return (
             <div className="baseClass">
-
-                <div>
-                    {c1 ?
-                        <Row>
-                            <Col span={8}>
-                                <div>
-                                    <div className="pageHead">Compare Items</div>
-                                    <div >
-                                        <span>{countSel}  items selected </span>
-                                    </div>
-                                    <div className="checkText">
-                                        {countSel == 2 ?
-                                            <Checkbox onChange={this.showDifferences}>
-                                                Show only differences
+                {f1.length > 0 ?
+                    <div>
+                        <div>
+                            {c1 ?
+                                <Row>
+                                    <Col span={8}>
+                                        <div>
+                                            <div className="pageHead">Compare Items</div>
+                                            <div >
+                                                <span>{countSel}  items selected </span>
+                                            </div>
+                                            <div className="checkText">
+                                                {countSel == 2 ?
+                                                    <Checkbox onChange={this.showDifferences}>
+                                                        Show only differences
                                         </Checkbox> : ""}
-                                    </div>
-                                </div>
-                            </Col>
-                            {/* Left Item */}
-                            <Col span={8}>
-                                <div>
-                                    {c1.productPricingSummary && leftDropdown ?
-                                        <div>
-                                            <div className="imgPlaceholder" />
-                                            <div>Add a product</div>
-                                            <Select placeholder="Choose a product" className="selectSection" onChange={this.handleLeftChange}>
-                                                {Object.keys(c1.productPricingSummary).map(item =>
-                                                    <Option value={item}> {item} </Option>
-                                                )}
-                                            </Select>
+                                            </div>
                                         </div>
-                                        :
+                                    </Col>
+                                    {/* Left Item */}
+                                    <Col span={8}>
                                         <div>
-                                            <CompareOf c1={c1} itemId={leftItem} />
-                                            <CloseCircleOutlined className="closeCompare" onClick={this.showLeftDropdown} />
+                                            {c1.productPricingSummary && leftDropdown ?
+                                                <div>
+                                                    <div className="imgPlaceholder" />
+                                                    <div>Add a product</div>
+                                                    <Select placeholder="Choose a product" className="selectSection" onChange={this.handleLeftChange}>
+                                                        {Object.keys(c1.productPricingSummary).map(item =>
+                                                            <Option value={item}> {item} </Option>
+                                                        )}
+                                                    </Select>
+                                                </div>
+                                                :
+                                                <div>
+                                                    <CompareOf c1={c1} itemId={leftItem} />
+                                                    <CloseCircleOutlined className="closeCompare" onClick={this.showLeftDropdown} />
+                                                </div>
+                                            }
                                         </div>
-                                    }
-                                </div>
-                            </Col>
-                            {/* Right Item */}
-                            <Col span={8}>
-                                <div>
-                                    {c1.productPricingSummary && rightDropdown ?
+                                    </Col>
+                                    {/* Right Item */}
+                                    <Col span={8}>
                                         <div>
-                                            <div className="imgPlaceholder" />
-                                            <div>Add a product</div>
-                                            <Select placeholder="Choose a product" className="selectSection" onChange={this.handleRightChange}>
-                                                {Object.keys(c1.productPricingSummary).map(item =>
-                                                    <Option value={item}> {item} </Option>
-                                                )}
-                                            </Select>
+                                            {c1.productPricingSummary && rightDropdown ?
+                                                <div>
+                                                    <div className="imgPlaceholder" />
+                                                    <div>Add a product</div>
+                                                    <Select placeholder="Choose a product" className="selectSection" onChange={this.handleRightChange}>
+                                                        {Object.keys(c1.productPricingSummary).map(item =>
+                                                            <Option value={item}> {item} </Option>
+                                                        )}
+                                                    </Select>
+                                                </div>
+                                                :
+                                                <div>
+                                                    <CompareOf c1={c1} itemId={rightItem} />
+                                                    <CloseCircleOutlined className="closeCompare" onClick={this.showRightDropdown} />
+                                                </div>
+                                            }
                                         </div>
-                                        :
-                                        <div>
-                                            <CompareOf c1={c1} itemId={rightItem} />
-                                            <CloseCircleOutlined className="closeCompare" onClick={this.showRightDropdown} />
-                                        </div>
-                                    }
-                                </div>
-                            </Col>
-                        </Row>
-                        : ""
-                    }
-                </div>
-                <div>
-                    {f1 ?
-                        f1.map(disp =>
-                            <div key={disp}>
-                                <div className="featureTitle">{disp.title}</div>
-                                {disp.features.map(item =>
-                                    <div>
-                                        {showOnlyDiff && (item.values[leftItem] == item.values[rightItem]) ? "" :
-                                            <Row key={item}>
-                                                <Col span={8} className="featureHeaderFont colRight leftPad">
-                                                    {item.featureName}
-                                                </Col>
-                                                {/* if checkbox show only differences */}
+                                    </Col>
+                                </Row>
+                                : ""
+                            }
+                        </div>
+                        <div>
+                            {f1 ?
+                                f1.map(disp =>
+                                    <div key={disp}>
+                                        <div className="featureTitle">{disp.title}</div>
+                                        {disp.features.map(item =>
+                                            <div>
+                                                {showOnlyDiff && (item.values[leftItem] == item.values[rightItem]) ? "" :
+                                                    <Row key={item}>
+                                                        <Col span={8} className="featureHeaderFont colRight leftPad">
+                                                            {item.featureName}
+                                                        </Col>
+                                                        {/* if checkbox show only differences */}
 
-                                                < Col span={8} className="colRight leftPad">
-                                                    {item.values[leftItem]}
-                                                </Col>
-                                                <Col span={8} className="leftPad">
-                                                    {item.values[rightItem]}
-                                                </Col>
-                                            </Row>
+                                                        < Col span={8} className="colRight leftPad">
+                                                            {item.values[leftItem]}
+                                                        </Col>
+                                                        <Col span={8} className="leftPad">
+                                                            {item.values[rightItem]}
+                                                        </Col>
+                                                    </Row>
+                                                }
+                                            </div>
+                                        )
                                         }
                                     </div>
                                 )
-                                }
-                            </div>
-                        )
-                        : ""
-                    }
-                </div>
+                                : ""
+                            }
+                        </div>
+                    </div>
+                    :
+                    <div className="loader">
+                        <div>
+                            <LoadingOutlined />
+                        </div>
+                                Loading, Please wait .......
+                    </div>
+                }
             </div>
         )
     }
