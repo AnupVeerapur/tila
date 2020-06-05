7113a3cf359bf2d9c7fb4d2b1af6c4fe2a436f04
AnupVeerapur <anupsveerapur@gmail.com>
2020-06-05 13:00:10 +0530

    Deploy gh-pages

--- next.config.js
+++ next.config.js
@@ -15,8 +15,7 @@ module.exports = ({
     },
     exportPathMap: function () { // /Next-React-Components
         return {
-            "/": { page: "/" },
-            "/index": { page: "/index" }
+            "/": { page: "/" }
         }
     },
     assetPrefix: assetPrefix,
