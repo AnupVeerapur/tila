d285ba290f5e3f250eac9e8f927d8167c47af5bc
AnupVeerapur <anupsveerapur@gmail.com>
2020-06-05 12:50:48 +0530

    Deploy gh-pages

---
+++ next.config.js
@@ -0,0 +1,36 @@
+// const withSass = require('@zeit/next-sass');
+// const withCSS = require("@zeit/next-css");
+
+/**
+ * withLess is added because we have imported antd.less instead of antd.css
+ * antd.css also can be used instead of antd.less
+ */
+// const withLess = require('@zeit/next-less');
+
+const assetPrefix = '/LayoutPOC';
+
+module.exports = ({
+    lessLoaderOptions: {
+        javascriptEnabled: true
+    },
+    exportPathMap: function () { // /Next-React-Components
+        return {
+            "/": { page: "/" },
+            "/index": { page: "/index" }
+        }
+    },
+    assetPrefix: assetPrefix,
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        // Note: we provide webpack above so you should not `require` it
+        // Perform customizations to webpack config
+        // Important: return the modified config
+        // config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
+
+        config.plugins.push(
+            new webpack.DefinePlugin({
+                'process.env.ASSET_PREFIX': JSON.stringify(assetPrefix),
+            }),
+        );
+        return config;
+    }
+});
